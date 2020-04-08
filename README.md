8450b46fcfdee6c31364659c28edcaf636cacf58
Ollie Power <powerollie@gmail.com>
2020-04-08 22:59:43 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # meme-generator
 A meme generator
+
+## Local Development
+
+Visual Studio Code.
+Live Server extension.
+
+## Acknowledgements
+
+- [Wild Code School meme generator](https://codepen.io/eveningkid/pen/qBdeQJo?editors=1010)
---
+++ static/js/script.js
@@ -0,0 +1,29 @@
+function updateText() {
+  const text = document.getElementById('user-text').value;
+  document.getElementById('meme-text').innerHTML = text;
+}
+
+function updateImage() {
+  const file = document.getElementById('user-picture').files[0];
+  document.getElementById('meme-picture').src = window.URL.createObjectURL(file);
+}
+
+function generateMeme() {
+  html2canvas(document.getElementById('meme'), { allowTaint: true }).then(function (canvas) {
+    const data = canvas.toDataURL('image/jpeg', 1);
+    const src = encodeURI(data);
+    document.getElementById('screenshot').src = src;
+  });
+}
+
+/* An alternative using .addEventListener, not 'onclick' on <button>
+document.getElementById('save-meme').addEventListener('click', function () {
+  // Same html2canvas code
+  html2canvas(document.getElementById('meme'), { allowTaint: true }).then(function (canvas) {
+    const data = canvas.toDataURL('image/jpeg', 1);
+    const src = encodeURI(data);
+    document.getElementById('screenshot').src = src;
+  });
+});
+*/
+  
